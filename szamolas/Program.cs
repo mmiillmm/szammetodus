25782d2b883670a7e49807da01f12550089fc79b
Ny19MakaiK <Ny19MakaiK@DESKTOP-12345>
2022-10-11 11:18:57 +0200

    Add project files.

---
+++ szamolas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"3 + 4 = {Szamol(3, 4, '+')} ");
+
+            Console.ReadKey();
+
+
+        }
+
+        static int Szamol(int x, int y, char op)
+        {
+            int eredmeny = 0;
+
+            switch (op)
+            {
+                case '+':
+                    eredmeny = x + y;
+                    break;
+            }
+
+            return eredmeny;
+        }
+    }
+}
